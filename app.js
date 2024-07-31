8d74b300a03d2345022b25207278ec6ec76d5dda
data-dev05 <169947116+data-dev05@users.noreply.github.com>
2024-07-31 15:23:49 -0600

    final version

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const buttonActive = document.getElementById("button-active");
 const buttonInactive = document.getElementById("button-inactive");
 const shareSectionActive = document.getElementById("div-active-mobile");
 const shareSectionInactive = document.getElementById("div-inactive-mobile");
+const shareSocialMedia = document.getElementById("share-active-desktop");
 
 buttonActive.addEventListener("click", () => {
-    shareSectionActive.style.display = "flex";
-    shareSectionInactive.style.display = "none";
+    if (window.innerWidth >= 765) {
+        if (shareSocialMedia.style.display === "none"){
+            shareSocialMedia.style.display = "flex"
+        } else {
+            shareSocialMedia.style.display = "none"
+        }
+    } else {
+        shareSectionActive.style.display = "flex";
+        shareSectionInactive.style.display = "none";
+    }
 });
 
 buttonInactive.addEventListener("click", () => {
